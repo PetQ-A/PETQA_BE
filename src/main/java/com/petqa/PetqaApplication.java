1bfc87e0bbe181a5406b785d2eb56b74cbe61348
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-07-09 09:21:48 +0900

    first commit

---
+++ src/main/java/com/petqa/PetqaApplication.java
@@ -0,0 +1,13 @@
+package com.petqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetqaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petqa/PetqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
