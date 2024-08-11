c035133878f032d71a883e32f58e36a81da2c164
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-08-11 14:33:24 +0900

    Fix: 엘라스틱 빈즈토크 에러

--- src/main/java/com/petqa/config/S3Config.java
+++ src/main/java/com/petqa/config/S3Config.java
@@ -1,10 +1,8 @@
 package com.petqa.config;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -24,11 +22,11 @@ public class S3Config {
 
     @Bean
     @ConditionalOnMissingBean(AmazonS3.class)
-    public AmazonS3Client amazonS3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
                 .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withForceGlobalBucketAccessEnabled(true)
                 .build();
     }
 }
