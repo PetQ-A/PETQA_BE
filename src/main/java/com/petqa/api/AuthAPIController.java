6c9fe2c877e5c6241817f362a70b051d98f60355
Park Dongkyu <86235780+dong99u@users.noreply.github.com>
2024-09-26 10:40:37 +0900

    Feature: 사용자 중복 여부 확인 API

--- src/main/java/com/petqa/api/AuthAPIController.java
+++ src/main/java/com/petqa/api/AuthAPIController.java
@@ -1,111 +1,122 @@
 package com.petqa.api;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.petqa.apiPayload.apiPayload.ApiResponse;
 import com.petqa.base.Util;
 import com.petqa.dto.auth.AuthRequestDTO;
 import com.petqa.dto.auth.AuthResponseDTO;
 import com.petqa.dto.user.UserRequestDTO;
 import com.petqa.service.user.UserCommandService;
+
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthAPIController {
 
-    private final UserCommandService userCommandService;
+	private final UserCommandService userCommandService;
 
-    @PostMapping("/login")
-    public ResponseEntity<ApiResponse<AuthResponseDTO.UserInfoDTO>> login(@RequestBody AuthRequestDTO.SocialLoginDTO loginRequest, HttpServletResponse response) {
-        AuthResponseDTO.LoginResponseDTO loginedUser = userCommandService.login(loginRequest);
+	@PostMapping("/login")
+	public ResponseEntity<ApiResponse<AuthResponseDTO.UserInfoDTO>> login(
+		@RequestBody AuthRequestDTO.SocialLoginDTO loginRequest, HttpServletResponse response) {
+		AuthResponseDTO.LoginResponseDTO loginedUser = userCommandService.login(loginRequest);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("access", loginedUser.getAccessToken());
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("access", loginedUser.getAccessToken());
 
-        Cookie refresh = Util.createCookie("refresh", loginedUser.getRefreshToken());
-        response.addCookie(refresh);
+		Cookie refresh = Util.createCookie("refresh", loginedUser.getRefreshToken());
+		response.addCookie(refresh);
 
-        return ResponseEntity.ok()
-                .headers(headers)
-                .body(ApiResponse.onSuccess(loginedUser.getUserInfo()));
-    }
+		return ResponseEntity.ok()
+			.headers(headers)
+			.body(ApiResponse.onSuccess(loginedUser.getUserInfo()));
+	}
 
-    @PostMapping(value = "/join", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<ApiResponse<AuthResponseDTO.UserInfoDTO>> join(
-            @RequestPart("joinRequest") UserRequestDTO.CreateUserDTO joinRequest,
-            @RequestPart(value = "petProfileImage", required = false) MultipartFile petProfileImage,
-            HttpServletResponse response) {
+	@PostMapping(value = "/join", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<ApiResponse<AuthResponseDTO.UserInfoDTO>> join(
+		@RequestPart("joinRequest") UserRequestDTO.CreateUserDTO joinRequest,
+		@RequestPart(value = "petProfileImage", required = false) MultipartFile petProfileImage,
+		HttpServletResponse response) {
 
-        joinRequest.setPetProfileImage(petProfileImage);
+		joinRequest.setPetProfileImage(petProfileImage);
 
+		AuthResponseDTO.LoginResponseDTO joinedUser = userCommandService.join(joinRequest);
 
-        AuthResponseDTO.LoginResponseDTO joinedUser = userCommandService.join(joinRequest);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("access", joinedUser.getAccessToken());
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("access", joinedUser.getAccessToken());
+		Cookie refresh = Util.createCookie("refresh", joinedUser.getRefreshToken());
+		response.addCookie(refresh);
 
-        Cookie refresh = Util.createCookie("refresh", joinedUser.getRefreshToken());
-        response.addCookie(refresh);
+		return ResponseEntity.ok()
+			.headers(headers)
+			.body(ApiResponse.onSuccess(joinedUser.getUserInfo()));
+	}
 
-        return ResponseEntity.ok()
-                .headers(headers)
-                .body(ApiResponse.onSuccess(joinedUser.getUserInfo()));
-    }
+	@PostMapping("/reissue")
+	public ResponseEntity<ApiResponse<String>> reissue(HttpServletRequest request, HttpServletResponse response) {
+		String refresh = null;
 
+		Cookie[] cookies = request.getCookies();
 
-    @PostMapping("/reissue")
-    public ResponseEntity<ApiResponse<String>> reissue(HttpServletRequest request, HttpServletResponse response) {
-        String refresh = null;
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals("refresh")) {
+				refresh = cookie.getValue();
+			}
+		}
 
-        Cookie[] cookies = request.getCookies();
+		AuthResponseDTO.ReissueResponseDTO reissued = userCommandService.reissue(refresh);
 
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("refresh")) {
-                refresh = cookie.getValue();
-            }
-        }
+		response.setHeader("access", reissued.getAccessToken());
+		response.addCookie(Util.createCookie("refresh", reissued.getRefreshToken()));
 
-        AuthResponseDTO.ReissueResponseDTO reissued = userCommandService.reissue(refresh);
+		return ResponseEntity.ok(ApiResponse.onSuccess("reissued"));
 
-        response.setHeader("access", reissued.getAccessToken());
-        response.addCookie(Util.createCookie("refresh", reissued.getRefreshToken()));
+	}
 
-        return ResponseEntity.ok(ApiResponse.onSuccess("reissued"));
+	@PostMapping("/logout")
+	public ResponseEntity<ApiResponse<String>> logout(HttpServletRequest request, HttpServletResponse response) {
+		String refreshToken = null;
+		Cookie[] cookies = request.getCookies();
 
-    }
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("refresh")) {
+					refreshToken = cookie.getValue();
+					break;
+				}
+			}
+		}
 
-    @PostMapping("/logout")
-    public ResponseEntity<ApiResponse<String>> logout(HttpServletRequest request, HttpServletResponse response) {
-        String refreshToken = null;
-        Cookie[] cookies = request.getCookies();
+		userCommandService.logout(refreshToken);
 
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("refresh")) {
-                    refreshToken = cookie.getValue();
-                    break;
-                }
-            }
-        }
+		// 클라이언트 쪽 쿠키 삭제
+		Cookie refreshCookie = new Cookie("refresh", null);
+		refreshCookie.setMaxAge(0);
+		refreshCookie.setPath("/");
+		response.addCookie(refreshCookie);
 
-        userCommandService.logout(refreshToken);
+		return ResponseEntity.ok(ApiResponse.onSuccess("로그아웃 성공"));
+	}
 
-        // 클라이언트 쪽 쿠키 삭제
-        Cookie refreshCookie = new Cookie("refresh", null);
-        refreshCookie.setMaxAge(0);
-        refreshCookie.setPath("/");
-        response.addCookie(refreshCookie);
-
-        return ResponseEntity.ok(ApiResponse.onSuccess("로그아웃 성공"));
-    }
+	@PostMapping("/duplicate")
+	public ApiResponse<String> checkDuplicate(@RequestBody @Valid UserRequestDTO.DuplicateCheckDTO duplicateCheckDTO) {
+		return ApiResponse.onSuccess(userCommandService.duplicateCheck(duplicateCheckDTO));
+	}
 }
 
 
--- src/main/java/com/petqa/repository/UserRepository.java
+++ src/main/java/com/petqa/repository/UserRepository.java
@@ -1,22 +1,24 @@
 package com.petqa.repository;
 
-import com.petqa.domain.User;
-import io.lettuce.core.dynamic.annotation.Param;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import com.petqa.domain.User;
+
+import io.lettuce.core.dynamic.annotation.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserBySocialIdAndUsername(String socialId, String username);
+	Optional<User> findUserBySocialIdAndUsername(String socialId, String username);
 
-    @Query("select u from User u join fetch u.pet where u.socialId = :socialId and u.username = :username")
-    Optional<User> findUserAndPetBySocialIdAndUsername(@Param("socialId") String socialId, @Param("username") String username);
+	@Query("select u from User u join fetch u.pet where u.socialId = :socialId and u.username = :username")
+	Optional<User> findUserAndPetBySocialIdAndUsername(@Param("socialId") String socialId,
+		@Param("username") String username);
 
+	Optional<User> findByUsername(String username);
 
-    Optional<User> findByUsername(String username);
+	Optional<User> findById(Long userId);
 
-    Optional<User> findById(Long userId);
-
-
+	Optional<User> findUserByUsername(String username);
 }
